cc72733e704d440ce2a2b70dbdcf0d9ce316ab36
Diana Drischel <drischeldev@gmail.com>
2024-03-18 16:59:16 -0300

    inclusao pasta images

---
+++ main.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('form-atividade');
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const inputNomeAtividade = getElementById('nome-atividade');
+    const inputNotaAtividade = getElementById('nota-atividade');
+
+    
+});
